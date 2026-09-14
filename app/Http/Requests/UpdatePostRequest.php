dbb22bf7437ccac8f93f04f237555dcf8ea45b74
AbdulBasitx19 <abdulbasitchaudhry106@gmail.com>
2026-09-14 19:39:04 +0500

    feat: Handeling Update Request by following SRP

---
+++ app/Http/Requests/UpdatePostRequest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdatePostRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'title' => 'required|string|max:255',
+            'description' => 'nullable|string',
+            'author' => 'sometimes|required|string|max:100',
+        ];
+    }
+
+    public function messages(): array
+    {
+       return [
+        'title.required' => "Post Title is required .",
+        'title.max' => "Post title must be less than 255 characters",
+        'author.required' => 'Author name is must.',
+       ];
+    }
+}
